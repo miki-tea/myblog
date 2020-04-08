2b378bffa897cb4c806a4b2cc86989375b4136df
miki <miki.ishii16@gmail.com>
2020-04-08 23:25:28 +0900

    固定ページ、プログ一覧ページ作成

--- loop.php
+++ loop.php
@@ -1,6 +1,7 @@
+<!-- 記事ループ -->
 <?php if(have_posts()): ?>
   <?php while(heve_posts()):the_post();?>
-    <article class="article">
+    <article class="article__item">
       <h2 class="article__title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
       <h3><?php the_authot_nickname(); ?>　<?php the_time("Y.m.j"); ?>　<?php single_cut_title('カテゴリー: '); ?></h3>
       <img src="" alt="">
@@ -9,4 +10,5 @@
       </p>
     </article>
   <?php endwhile; ?>
-  <?php endif; ?>
+<?php endif; ?>
+<!-- 記事ループ終わり -->
--- profile.php
+++ profile.php
@@ -7,8 +7,28 @@ Template Name: Profile -プロフィールページ-
 <?php get_header(); ?>
     <!-- メニュー読み込み -->
     <?php get_template_part('content','menu'); ?>
-    <main class="main">
-      <div class="container">ここはメイン部分です。</div>
-    </main>
+    <div class="main">
+      <section id="profile">
+        <h1 class="title">
+          <?php echo get_the_title(); ?>
+        </h1>
+        <div class="content">
+          <?php echo get_post_meta($post->ID, 'profile', true); ?>
+        </div>
+      </section>
+      <section id="prof_info" class="site-eidth">
+        <?php if(have_posts()) :
+          while (have_posts()) : the_post(); ?>
+            <div id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+              <?php the_content(); ?>
+            </div>
+          <?php endwhile;
+          else : ?>
+            <div class="post">
+              <p>お探しの記事は見つかりませんでした。</p>
+            </div>
+          <?php endif; ?>
+      </section>
+    </div>
 
 <?php get_footer(); ?>
